30183578a4e34cac7e9528488be3f39f947855c2
edu011ms-ops <eduardo18matos@gmail.com>
2026-04-24 00:44:46 -0300

    Enhance transport selection and validation logic
    Added user checkbox to select transport options and handle empty data case.

--- app.py
+++ app.py
@@ -4,53 +4,59 @@ import matplotlib.pyplot as plt
 
 st.title("🚗 Calculadora de Transporte Mais Vantajoso")
 
-# 🆕 Distância
+# Distância
 distancia = st.number_input("Distância até o destino (km)", min_value=1.0, value=5.0)
 
-# Entrada do usuário
+# Lista de transportes
 transportes = ["ônibus", "carro", "bicicleta", "uber"]
 
 dados = {}
 
-st.subheader("Insira os dados:")
+st.subheader("Selecione e insira os dados:")
 
 for t in transportes:
-    st.write(f"### {t}")
-    tempo = st.number_input(f"Tempo (min) - {t}", min_value=0, key=t+"tempo")
-    custo = st.number_input(f"Custo base (R$) - {t}", min_value=0.0, key=t+"custo")
+    usar = st.checkbox(f"Usar {t}", value=True)
     
-    # 🆕 ajustando custo pela distância
-    custo_total = custo * distancia
-    
-    dados[t] = {"tempo": tempo, "custo": custo_total}
+    if usar:
+        st.write(f"### {t}")
+        tempo = st.number_input(f"Tempo (min) - {t}", min_value=0, key=t+"tempo")
+        custo = st.number_input(f"Custo base (R$) - {t}", min_value=0.0, key=t+"custo")
+        
+        custo_total = custo * distancia
+        
+        dados[t] = {"tempo": tempo, "custo": custo_total}
 
-# Peso personalizado
+# Preferência do usuário
 st.subheader("Preferência do usuário")
 peso_custo = st.slider("Importância do custo", 0.0, 1.0, 0.6)
 peso_tempo = 1 - peso_custo
 
 if st.button("Calcular melhor opção"):
-    nomes = list(dados.keys())
-    custos = np.array([dados[n]["custo"] for n in nomes])
-    tempos = np.array([dados[n]["tempo"] for n in nomes])
+    
+    if len(dados) == 0:
+        st.warning("Selecione pelo menos um meio de transporte!")
+    else:
+        nomes = list(dados.keys())
+        custos = np.array([dados[n]["custo"] for n in nomes])
+        tempos = np.array([dados[n]["tempo"] for n in nomes])
 
-    vantagens = custos * peso_custo + tempos * peso_tempo
+        vantagens = custos * peso_custo + tempos * peso_tempo
 
-    melhor_idx = np.argmin(vantagens)
+        melhor_idx = np.argmin(vantagens)
 
-    st.success(f"Melhor opção: {nomes[melhor_idx]}")
+        st.success(f"Melhor opção: {nomes[melhor_idx]}")
 
-    # Gráfico
-    fig, ax = plt.subplots()
-    ax.scatter(tempos, custos)
+        # Gráfico
+        fig, ax = plt.subplots()
+        ax.scatter(tempos, custos)
 
-    # destacar melhor
-    ax.scatter(tempos[melhor_idx], custos[melhor_idx], s=200)
+        # destacar melhor
+        ax.scatter(tempos[melhor_idx], custos[melhor_idx], s=200)
 
-    for i in range(len(nomes)):
-        ax.text(tempos[i], custos[i], nomes[i])
+        for i in range(len(nomes)):
+            ax.text(tempos[i], custos[i], nomes[i])
 
-    ax.set_xlabel("Tempo")
-    ax.set_ylabel("Custo")
+        ax.set_xlabel("Tempo")
+        ax.set_ylabel("Custo")
 
-    st.pyplot(fig)
+        st.pyplot(fig)
